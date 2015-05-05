6747d7c069342822b93ee76608b1711699b4c138
Oscar <otech47@gmail.com>
2015-05-05 12:54:15 -0700

    scripts
    timed social media script in progress

---
+++ controllers/scripts.js
@@ -0,0 +1,29 @@
+// Handles relations w/ mysql table artist_social_media in schema setstory
+var settings = require('../config/settings');
+var mysql = require('mysql');
+var connection = mysql.createPool(settings.db.setstory);
+var rest = require('restler');
+
+var scripts = {};
+
+var artists = ["Taylor Swift", "AWOLNATION", "Coldplay", "Skrillex"];
+
+scripts.startTimedSocialMedia = function(cb) {
+	console.log("Starting timed social media script...")
+	
+
+	for(var i in artists) {
+		console.log("Make social media request");
+		var requestThresholdMin = 20000; // at least 20 seconds between requests
+		var requestThresholdMax = 35000; // at most 35 seconds between requests
+
+		var timer = Math.floor((Math.random() * requestThresholdMax) + requestThresholdMin);
+		setTimeout()
+	}
+
+	cb();
+
+	function get
+}
+
+module.exports = scripts;
