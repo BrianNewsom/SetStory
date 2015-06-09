86b5b2444bf8a3b8e9a57f96633f01c862438c56
martin rabaglia <martinrabaglia@martins-MacBook-Pro.local>
2015-06-09 00:37:17 -0300

    all social graph hooked in on artists

--- public/scripts/ng/main.js
+++ public/scripts/ng/main.js
@@ -67,57 +67,58 @@ myApp.controller('SearchController', function($scope,$rootScope,$location, $http
 
 myApp.controller('ArtistsController', function($scope,$interval, $filter, $sce,$filter, $rootScope,$routeParams,$location, $http) {
 	
-
+	$scope.choice = $routeParams.name;
 	var plays = []
 
 	$scope.getArtistScore = function() {
 
-    	// var url = '/api/artist/avascore/' + $scope.choice;
+    	var url = '/api/artist/avascore/' + $scope.choice;
 
-    	// console.log(url);
+    	console.log(url);
 
-    	// $http.get(url).success(function(metadata) {
-    	// 	console.log(metadata);
-    	// 	$scope.artistScore = metadata.ava_score || 12345;
-    	// });
-		$scope.artistScore = 1234567
+    	$http.get(url).success(function(metadata) {
+    		console.log(metadata);
+    		$scope.artistScore = metadata.ava_score || 12345;
+    	});
+		
 
     }
 
     $scope.getArtistScore();
 
-    $scope.getArtistSocialData = function() {
-    	// var url = '/api/artist/avascore/' + $scope.choice;
 
-    	// console.log(url);
-
-    	// $http.get(url).success(function(metadata) {
-    	// 	console.log(metadata);
-    	// 	$scope.artistScore = metadata.ava_score || 12345;
-    	// });
-		$scope.socialData = {
-			facebook_followers: 100,
-			twitter_followers: 200,
-			instagram_followers: 300,
-			soundcloud_followers: 400,
-			youtube_followers: 500,
-		}
-
-    }
-
-    $scope.getArtistSocialData();
 
     $scope.getArtistMediaData = function() {
-    	// var url = '/api/artist/avascore/' + $scope.choice;
+    	var url = 'http://setmine.com/api/v/7/artist/metrics/social/' + $scope.choice;
 
-    	// console.log(url);
+    	console.log(url);
 
-    	// $http.get(url).success(function(metadata) {
-    	// 	console.log(metadata);
-    	// 	$scope.artistScore = metadata.ava_score || 12345;
-    	// });
+    	$http.get(url).success(function(metadata) {
+    	
+    	console.log(metadata);
+    	
+    	var social = {
+			facebook_followers: resumedNumberStringToNumber(metadata.payload.metrics.social.facebook.total),
+			twitter_followers: resumedNumberStringToNumber(metadata.payload.metrics.social.twitter.total),
+			instagram_followers: resumedNumberStringToNumber(metadata.payload.metrics.social.instagram.total),
+			soundcloud_followers: resumedNumberStringToNumber(metadata.payload.metrics.social.soundcloud.total_plays),
+			youtube_followers: resumedNumberStringToNumber(metadata.payload.metrics.social.youtube.total_plays),
+		}
 
-		$scope.playsOverTime = {
+
+
+		
+
+	    var data = [];
+		    data.push({number: 1, value: social.facebook_followers, name: "facebook", max: 10000000 * 1.2});
+		    data.push({number: 2, value: social.twitter_followers, name: "twitter", max: 70697964 * 1.2});
+		    data.push({number: 3, value: social.instagram_followers, name: "instagram", max: 356000000});
+		    data.push({number: 4, value: social.soundcloud_followers, name: "soundcloud", max: 100000000});
+		    data.push({number: 5, value: social.youtube_followers,name:"youtube", max: 30000000000});
+		 $scope.socialset = data;
+
+
+		 $scope.playsOverTime = {
 			setmine: [
 				2000,
 				3000,
@@ -137,6 +138,42 @@ myApp.controller('ArtistsController', function($scope,$interval, $filter, $sce,$
 				5800
 			],
 		}
+	var setmineOverTime =[];
+	var youtubeOverTime =[];
+	var soundcloudOverTime =[];
+
+
+	maxYoutube = 100000;
+ 	maxSoundCloud = 500000;
+	//TODO : INTEGRATE SETMINE!
+	setmineOverTime.push({name:"FEB", plays: $scope.playsOverTime.setmine[0]});
+	setmineOverTime.push({name:"MAR", plays: $scope.playsOverTime.setmine[1]});
+	setmineOverTime.push({name:"APR", plays: $scope.playsOverTime.setmine[2]});
+	setmineOverTime.push({name:"MAY", plays: $scope.playsOverTime.setmine[3]});
+
+
+	youtubeOverTime.push({name:"FEB",  plays: metadata.payload.metrics.social.youtube.overtime[1][1]});
+	youtubeOverTime.push({name:"MAR", plays:  metadata.payload.metrics.social.youtube.overtime[2][1]});
+	youtubeOverTime.push({name:"APR", plays:  metadata.payload.metrics.social.youtube.overtime[3][1]});
+	youtubeOverTime.push({name:"MAY", plays:  metadata.payload.metrics.social.youtube.overtime[4][1]});
+
+
+ 	soundcloudOverTime.push({name:"FEB", plays: metadata.payload.metrics.social.soundcloud.overtime[1][1]});
+	soundcloudOverTime.push({name:"MAR", plays:  metadata.payload.metrics.social.soundcloud.overtime[2][1]});
+	soundcloudOverTime.push({name:"APR", plays:  metadata.payload.metrics.social.soundcloud.overtime[3][1]});
+	soundcloudOverTime.push({name:"MAY", plays:  metadata.payload.metrics.social.soundcloud.overtime[4][1]});
+
+	plays.push({name:'setmine', max :maxSetMine, months:setmineOverTime});
+	plays.push({name:'youtube', max: maxYoutube, months:youtubeOverTime});
+	plays.push({name:'soundcloud', max:maxSoundCloud, months:soundcloudOverTime});
+
+	$scope.plays = plays;
+
+
+
+    	});
+
+		
 
     }
 
@@ -163,62 +200,8 @@ myApp.controller('ArtistsController', function($scope,$interval, $filter, $sce,$
 		return format(play);
 	};
 
-	var setmineOverTime =[];
-	var youtubeOverTime =[];
-	var soundcloudOverTime =[];
 
 
-	setmineOverTime.push({name:"FEB", plays: $scope.playsOverTime.setmine[0]});
-	setmineOverTime.push({name:"MAR", plays: $scope.playsOverTime.setmine[1]});
-	setmineOverTime.push({name:"APR", plays: $scope.playsOverTime.setmine[2]});
-	setmineOverTime.push({name:"MAY", plays: $scope.playsOverTime.setmine[3]});
-
-	youtubeOverTime.push({name:"FEB", plays: $scope.playsOverTime.youtube[0]});
-	youtubeOverTime.push({name:"MAR", plays: $scope.playsOverTime.youtube[1]});
-	youtubeOverTime.push({name:"APR", plays: $scope.playsOverTime.youtube[2]});
-	youtubeOverTime.push({name:"MAY", plays: $scope.playsOverTime.youtube[3]});
-
-	soundcloudOverTime.push({name:"FEB", plays: $scope.playsOverTime.soundcloud[0]});
-	soundcloudOverTime.push({name:"MAR", plays: $scope.playsOverTime.soundcloud[1]});
-	soundcloudOverTime.push({name:"APR", plays: $scope.playsOverTime.soundcloud[2]});
-	soundcloudOverTime.push({name:"MAY", plays: $scope.playsOverTime.soundcloud[3]});
-
-	plays.push({name:'setmine', max :maxSetMine, months:setmineOverTime});
-	plays.push({name:'youtube', max: maxYoutube, months:youtubeOverTime});
-	plays.push({name:'soundcloud', max:maxSoundCloud, months:soundcloudOverTime});
-	
-	$scope.plays = plays;
-
-	$scope.socialset =
-	    [{"number":1, "value":20, "max":20},
-	    {"number":2, "value":20, "max":20},
-	    {"number":3, "value":20, "max":20},
-	    {"number":4, "value":20, "max":20},
-	    {"number":5, "value":20, "max":20}]
-
-    var sources = [];
-	    sources.push({name: "facebook", max: 100000000 * 1.2});
-	    sources.push({name: "twitter", max: 70697964 * 1.2});
-	    sources.push({name: "youtube", max: 30000000000});
-	    sources.push({name: "soundcloud", max: 1000000});
-	    sources.push({name: "setmine", max: 1000000});
-
-
-	$interval(function(){
-		  var data = [];
-      for(var i = 0; i < sources.length; i++) {
-      	var s = sources[i]
-        data.push({
-        	"name": s.name, 
-        	"number":i, 
-        	"max": s.max,
-        	"value": Math.floor(Math.random()*s.max)});
-    	}
-
-    	$scope.socialset = data;  
-  	},3000);
-
-	$scope.choice = $routeParams.name;
 
 	$scope.getArtistID = function() {
 		var url = "api/getArtistID/" + $scope.choice; 
@@ -463,6 +446,32 @@ function processArtistImage(artist){
 	}
 	else {
 		artist.imageURL = 'http://stredm.s3-website-us-east-1.amazonaws.com/namecheap/' +  artist.imageURL;	
+	}
+
+}
+
+
+function resumedNumberStringToNumber(a){
+	if (!a){
+		return 0;
+	}
+	var symbol = a.split('').reverse()[0].toLowerCase();
+	console.log(symbol);
+	if (symbol != 'm' &&  symbol != 'k'){
+		return parseFloat(a);
+	}
+	else {
+		var number = parseFloat(a.replace(symbol,'') + 0);
+
+		var power = 1;
+		if (symbol === 'm'){
+			power = 1000000;
+		}
+		else if (symbol === 'k'){
+			power = 1000;
+		}
+		console.log(number ,power);
+		return number * power;
 	}
 
 }
