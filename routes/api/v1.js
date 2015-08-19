0a7d16fab26852d89c43f5509ed7a148ea37e1a1
Oscar Lafarga <otech47@gmail.com>
2015-08-19 05:02:01 -0400

    directories

--- routes/api/v1.js
+++ routes/api/v1.js
@@ -1,19 +1,19 @@
 var express = require('express');
 var router = express.Router();
-var skrillex = require('../data/Skrillex.json');
-var decibel = require('../apiHandlers/decibel');
-var openaura = require('../apiHandlers/openaura');
-var setlistFM = require('../apiHandlers/setlistfm');
-var unified = require('../apiHandlers/unified');
-var musicgraph = require('../apiHandlers/musicgraph');
-var setmine = require('../apiHandlers/setmine')
-var soundcloud = require('../apiHandlers/soundcloud')
-var instagram = require('../apiHandlers/instagram')
-var stubhub = require('../apiHandlers/stubhub')
+var skrillex = require('../../data/Skrillex.json');
+var decibel = require('../../apiHandlers/decibel');
+var openaura = require('../../apiHandlers/openaura');
+var setlistFM = require('../../apiHandlers/setlistfm');
+var unified = require('../../apiHandlers/unified');
+var musicgraph = require('../../apiHandlers/musicgraph');
+var setmine = require('../../apiHandlers/setmine')
+var soundcloud = require('../../apiHandlers/soundcloud')
+var instagram = require('../../apiHandlers/instagram')
+var stubhub = require('../../apiHandlers/stubhub')
 
-var echonest = require('../apiHandlers/echonest')
-var artist_social_media = require('../controllers/artist_social_media');
-var artist_media_plays = require('../controllers/artist_media_plays');
+var echonest = require('../../apiHandlers/echonest')
+var artist_social_media = require('../../controllers/artist_social_media');
+var artist_media_plays = require('../../controllers/artist_media_plays');
 var winston = require('winston');
 var ava;
 var eva;
@@ -23,8 +23,8 @@ var jf = require('jsonfile')
 
 setmine.init(function() {
     winston.info("setmine models stored.")
-    ava = require('../controllers/ava')
-    eva = require('../controllers/eva')
+    ava = require('../../controllers/ava')
+    eva = require('../../controllers/eva')
 })
 
 router.get('/artist/social_media', function(req, res, next){
